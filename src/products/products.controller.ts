9d40e79f8e8db4326ad68d74f7b4174b548a395b
Adzahari <wnabwt1992@gmail.com>
2025-07-08 00:28:56 +0800

    feat: initialize NestJS application with basic structure and product management
    - Added package.json with dependencies and scripts for building, testing, and linting.
    - Created AppController and AppService with a simple "Hello World!" endpoint.
    - Implemented ProductsModule, ProductsController, and ProductsService for product management.
    - Added models and service methods for CRUD operations on products.
    - Included unit tests for controllers and services.
    - Set up end-to-end tests for the application.
    - Configured TypeScript settings with tsconfig.json and tsconfig.build.json.

---
+++ src/products/products.controller.ts
@@ -0,0 +1,45 @@
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	ParseIntPipe,
+	Patch,
+	Post,
+} from "@nestjs/common"
+import type { Product } from "./models/products.model"
+import type { ProductsService } from "./products.service"
+
+@Controller("products")
+export class ProductsController {
+	constructor(private readonly productsService: ProductsService) {}
+
+	@Get()
+	findAll(): Product[] {
+		return this.productsService.findAll()
+	}
+
+	@Get(":id")
+	findOne(@Param("id", ParseIntPipe) id: number): Product {
+		return this.productsService.findOne(id)
+	}
+
+	@Post()
+	create(@Body() body: Omit<Product, "id">): Product {
+		return this.productsService.create(body)
+	}
+
+	@Patch(":id")
+	update(
+		@Param("id", ParseIntPipe) id: number,
+		@Body() body: Partial<Omit<Product, "id">>,
+	): Product {
+		return this.productsService.update(id, body)
+	}
+
+	@Delete(":id")
+	remove(@Param("id", ParseIntPipe) id: number): void {
+		this.productsService.remove(id)
+	}
+}
---
+++ src/products/products.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, NotFoundException } from "@nestjs/common"
+import type { Product } from "./models/products.model"
+
+@Injectable()
+export class ProductsService {
+	private products: Product[] = []
+	private nextId = 1
+
+	findAll(): Product[] {
+		return this.products
+	}
+
+	findOne(id: number): Product {
+		const product = this.products.find((p) => p.id === id)
+
+		if (!product) {
+			throw new NotFoundException("Product not found")
+		}
+
+		return product
+	}
+
+	create(data: Omit<Product, "id">): Product {
+		const product: Product = {
+			...data,
+			id: this.nextId++,
+		}
+
+		this.products.push(product)
+
+		return product
+	}
+
+	update(id: number, data: Partial<Omit<Product, "id">>): Product {
+		const product = this.products.find((p) => p.id === id)
+
+		if (!product) {
+			throw new NotFoundException("Product not found")
+		}
+
+		Object.assign(product, data)
+
+		return product
+	}
+
+	remove(id: number): void {
+		const updatedProducts = this.products.filter((p) => p.id !== id)
+		this.products = updatedProducts
+	}
+}
